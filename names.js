b16a91bf33806c8f53078745ac000bf0f4bfd508
mattiapennisi <mattiapennisi0@gmail.com>
2025-03-10 13:05:49 +0100

    add names function and export it

---
+++ names.js
@@ -0,0 +1,10 @@
+function createFullnameObject (firstName, lastName) {
+    fullnameObject = {
+        firstName,
+        lastName
+    }
+
+    return fullnameObject
+}
+
+module.exports = {createFullnameObject}
